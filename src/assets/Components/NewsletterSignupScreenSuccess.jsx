303ebf6a79ecd1c5d29b381486596d21670d9c34
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-15 12:26:57 +0300

    src klasoru icindeki assets klasoru icindeki components klasoru icinde yer alan newslettersignupscreensuccess.jsx component icinde oval ve check eklendi, css duzenlemesi yapildi

--- src/assets/Components/NewsletterSignupScreenSuccess.jsx
+++ src/assets/Components/NewsletterSignupScreenSuccess.jsx
@@ -1,7 +1,14 @@
 import OvalBig from "../img/oval-big.png"
+import CheckBig from "../img/check-big.png"
+import "../../App"
 
 export default function NewsletterSignupScreenSuccess() {
-  return(
-    <img src={OvalBig} className="oval-big" alt="resim" />
+  return (
+    <>
+      <div className="check-and-oval">
+        <img src={CheckBig} className="check-big" alt="resim" />
+        <img src={OvalBig} className="oval-big" alt="resim" />
+      </div>
+    </>
   )
 }
